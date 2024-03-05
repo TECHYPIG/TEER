6162876ee7c3dc62983f9ca14303fdfdd95415d7
Charlee Wood <charleewood62@gmail.com>
2024-03-05 17:11:53 +0000

    edited

--- app/homepage/page.js
+++ app/homepage/page.js
@@ -1,36 +1,36 @@
-import "./home.css";
+
 import Image from "next/image";
+import Piggy from "./piggy.jpg";
+import styles from './Home.module.css';
 
 export default function Home() {
   return (
-    <main className="home-container">
-      <div className="navbar">
-        <p className="nav-txt">
+    <main className={styles.homecontainer}>
+      <div className={styles.navbar}>
+        <p className={styles.navtxt}>
           <a href="#homepage">TEER</a>
         </p>
-        <p className="Search">
+        <p className={styles.Search}>
         Search:
         </p>
       </div>
-      <div className="side-profile">
-      <p className="Profile-picture">
-      <Image
-              src=""
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-        </p>
-        <p className="User-details">
-        
-        </p>
-        <p className="socials">
-          <a href="#instagram">@instagram</a>
-          <a href="#X">@X</a>
-          <a href="#LinkedIn">@LinkedIn</a>
-        </p>
+      <div className={styles.sideprofile}>
+        <div className={styles.userinfo}>
+          <Image src={Piggy} className={styles.profilepic} width={30} height={30} alt='Piggy' />
+            <div className={styles.usernames}>
+            <h1>Amy Solar</h1>
+            <h2>@amysolar</h2>
+          </div>
+        </div>
+
+          <p className="User-details">
+          
+          </p>
+          <p className="socials">
+            <a href="#instagram">@instagram</a>
+            <a href="#X">@X</a>
+            <a href="#LinkedIn">@LinkedIn</a>
+          </p>
       </div>
     </main>
   );

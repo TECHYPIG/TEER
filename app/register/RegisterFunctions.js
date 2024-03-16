abe5c02a43656dfc3a2ad422511bd05e289368f2
jugtejSingh <jugtej4singh@gmail.com>
2024-03-16 04:03:50 +0000

    Login and Register
    Both work, A little bit of more restraints need to be added and some more front end.

--- app/register/RegisterFunctions.js
+++ app/register/RegisterFunctions.js
@@ -2,6 +2,7 @@
 import "./RegisterFunctions.css"
 import {useState} from "react";
 import crypto from "crypto"
+import {locations} from "./countries"
 
 export function RegisterSign(){
     return(
@@ -15,9 +16,11 @@ export function RegisterInputs(){
     const [username,changedUsername] = useState("")
     const [password,changedPassword] = useState("")
     const [email,changedEmail] = useState("")
-    const [firstName,changedFirstName] = useState("")
+    const [fullName,changedFullname] = useState("")
     const [confirmPassword,changedConfirmPassword] = useState("")
-    const [age,changedAge] = useState("")
+    const [location,changedLocation] = useState("")
+    const [gender,changedGender] = useState("")
+    const [birthDate,changedBirthDate] = useState("")
     const [dontMatch, ChangedDontMatch] = useState("")
 
     return(
@@ -36,33 +39,48 @@ export function RegisterInputs(){
                             onChange ={event => onChangeFunction(event,changedEmail)}/>
                  </div>
                  <div className="Flex">
+                     <label>Location</label>
+                     <select  className="Input" onChange ={event => onChangeFunction(event,changedLocation)}>
+                         {locations}
+                     </select>
+                 </div>
+                 <div className="Flex">
                      <label>Password</label>
                      <input type="text" className="Input" value={password}
                             onChange ={event => onChangeFunction(event,changedPassword)}/>
+
                  </div>
             </div>
 
             <div className="Right">
                 <div className="Flex">
-                    <label>First Name</label>
-                    <input type="text" className="Input" value={firstName}
-                           onChange ={event => onChangeFunction(event,changedFirstName)}/>
+                    <label>Full Name</label>
+                    <input type="text" className="Input" value={fullName}
+                           onChange ={event => onChangeFunction(event,changedFullname)}/>
                 </div>
                 <div className="Flex">
-                    <label>Age</label>
-                    <input type="text" className="Input" value={age}
-                           onChange ={event => onChangeFunction(event,changedAge)}/>
+                    <label>Birth Date</label>
+                    <input type="date" className="Input" onChange ={event => onChangeFunction(event,changedBirthDate)}/>
                 </div>
+                    <div className="Flex">
+                        <label>Gender</label>
+                        <select className="Input" value={gender} onChange ={event => onChangeFunction(event,changedGender)}>
+                            <option key="Male">Male</option>
+                            <option key="Female">Female</option>
+                            <option key="Others">Others</option>
+                            <option key="PreferNotToSay">Prefer Not To Say</option>
+                        </select>
+                    </div>
                 <div className="Flex">
                     <label>Confirm Password</label>
                     <input type="text" className="Input" value={confirmPassword}
                            onChange ={event => onChangeFunction(event,changedConfirmPassword)}/>
                 </div>
+                </div>
             </div>
-        </div>
             <div className="passwordsDontMatch"><h5>{dontMatch}</h5></div>
        <input type="submit" onClick={async function inside (){
-           var value = await registerInformation(username,password,confirmPassword,age,firstName,email,ChangedDontMatch);
+           let value = await registerInformation(username,location, password,confirmPassword,birthDate,fullName,gender,email,ChangedDontMatch);
            switch (value) {
                case 0:
                    ChangedDontMatch("Your passwords do not match")
@@ -77,13 +95,19 @@ export function RegisterInputs(){
                    ChangedDontMatch("Your Confirmed Password is empty");
                    break;
                case 40:
-                   ChangedDontMatch("Age is empty");
+                   ChangedDontMatch("Birth Date is empty");
                    break;
                case 50:
-                   ChangedDontMatch("First name is empty");
+                   ChangedDontMatch("Your name is empty");
                    break;
                case 60:
-                   ChangedDontMatch("email is empty");
+                   ChangedDontMatch("Location is empty");
+                   break;
+               case 70:
+                   ChangedDontMatch("Gender is empty");
+                   break;
+               case 80:
+                   ChangedDontMatch("Email is empty");
                    break;
            }
        }}
@@ -98,12 +122,9 @@ function onChangeFunction(event, inputState){
 function onChangeFunctionUsername(event, inputState){
     let x = event.target.value
     inputState(x)
-    var matches = x.match(/\d+/g);
-    if (matches != null) {
-        
-    }
 }
-async function registerInformation(username,password,confirmPassword,age,firstName,email,ChangedDontMatch){
+async function registerInformation(username,location, password,confirmPassword,birthDate,fullName,gender,email,ChangedDontMatch){
+    console.log("Working")
     if(username === ""){
         return 10
     }
@@ -113,32 +134,54 @@ async function registerInformation(username,password,confirmPassword,age,firstNa
     if(confirmPassword === ""){
         return 30
     }
-    if(age === ""){
-        return 40
-    }
-    if(firstName === ""){
+    // if(birthDate === ){
+    //     return 40
+    // }
+    if(fullName === ""){
         return 50
     }
-    if(email === ""){
+    if(location === "Select One"){
         return 60
+    }
+    if(gender === ""){
+        return 70
+    }
+    if(email === ""){
+        return 80
+    }
+    var firstname
+    var surname
+    let nameSplit = fullName.split(" ");
+    if(nameSplit[1] === undefined){
+        return 100
+    } else{
+       firstname = nameSplit[0];
+        surname = nameSplit[1];
     }
 
     var response;
     if (password !== confirmPassword){
         return 0;
     } else{
+        console.log("testing")
         ChangedDontMatch("")
         let hashedPassword = crypto.createHash("sha256").update(password).digest('hex')
+
             try{
                 response =  await fetch('/api/register', {
                     method: 'POST', // Adjust the method as needed (GET, POST, etc.)
                     'Content-Type': 'application/json',
                     body: JSON.stringify({username : username,
                                                 password : hashedPassword,
-                                                age : age,
-                                                firstName : firstName,
-                                                email : email})
+                                                firstname : firstname,
+                                                surname : surname,
+                                                email : email,
+                                                birthDate : birthDate,
+                                                gender : gender,
+                                                location : location})
                 });
+                console.log("returnedValue")
+                console.log(response)
             }
             catch (error){
                 console.log(error);

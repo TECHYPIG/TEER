1012184cb1cdf32a311eb8bf410dd559281365b2
Charlee Wood <charleewood62@gmail.com>
2024-03-22 16:12:31 +0000

    Delete Post from homepage

--- app/home/page.js
+++ app/home/page.js
@@ -100,6 +100,8 @@ export default function Home() {
                 <Post
                   key={index}
                   post={post}
+                  setPosts={setPosts}
+                  posts={posts}
                   userDetails={userDetails}
                   CommentSuccess={CommentSuccess}
                   CommentError={CommentError}
